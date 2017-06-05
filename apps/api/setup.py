01f86cafa448e37dbf79078864067cb6642f5ba7
Nathan Bigaignon <nathanbigaignon@gmail.com>
2017-06-05 17:31:41 +0100

    Add base files

---
+++ apps/api/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='api',
+    packages=['api'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
